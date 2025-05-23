796b4636001c9b1609f79db7230646ffa2540e7c
jambox <joshuacohen2019@u.northwestern.edu>
2025-05-22 21:49:46 -0700

    got a rectangle to c code parser with brandongit add -u

--- booth_controller/lighting_array_controller.cpp
+++ booth_controller/lighting_array_controller.cpp
@@ -13,22 +13,57 @@ void LightingArrayController::setup(){
   FastLED.setBrightness( 255 );
 
   // setup rectangles
-  rects_[0] = Rectangle(0, 18); // 1st
-  rects_[1] = Rectangle(18, 16); // 2nd
-  rects_[2] = Rectangle(34, 12); // 3rd 
-  rects_[3] = Rectangle(46, 12); // 4th
-  rects_[4] = Rectangle(58, 24); // 5th
-  rects_[5] = Rectangle(82, 42); // 6th
-  rects_[6] = Rectangle(124, 22); // 7th 
-  rects_[7] = Rectangle(146, 17); // 8th
-  rects_[8] = Rectangle(163, 16); // 9th
-  rects_[9] = Rectangle(179, 18); // 10th 
-  rects_[10] = Rectangle(197, 18); // 11th
-  rects_[11] = Rectangle(215, 12); // 12th 
-  rects_[12] = Rectangle(227, 12); // 13th 
-  rects_[13] = Rectangle(239, 30); // 14th
-
-
+  rects_[0] = Rectangle(0,18);
+  rects_[1] = Rectangle(18,16);
+  rects_[2] = Rectangle(34,12);
+  rects_[3] = Rectangle(46,12);
+  rects_[4] = Rectangle(58,24);
+  rects_[5] = Rectangle(82,42);
+  rects_[6] = Rectangle(124,22);
+  rects_[7] = Rectangle(146,18);
+  rects_[8] = Rectangle(164,16);
+  rects_[9] = Rectangle(180,18);
+  rects_[10] = Rectangle(198,18);
+  rects_[11] = Rectangle(216,12);
+  rects_[12] = Rectangle(228,12);
+  rects_[13] = Rectangle(240,30);
+  rects_[14] = Rectangle(270,12);
+  rects_[15] = Rectangle(282,14);
+  rects_[16] = Rectangle(296,34);
+  rects_[17] = Rectangle(330,30);
+  rects_[18] = Rectangle(360,48);
+  rects_[19] = Rectangle(408,14);
+  rects_[20] = Rectangle(422,76);
+  rects_[21] = Rectangle(498,22);
+  rects_[22] = Rectangle(520,36);
+  rects_[23] = Rectangle(556,30);
+  rects_[24] = Rectangle(586,36);
+  rects_[25] = Rectangle(622,9);
+  rects_[26] = Rectangle(631,18);
+  rects_[27] = Rectangle(649,30);
+  rects_[28] = Rectangle(679,22);
+  rects_[29] = Rectangle(701,16);
+  rects_[30] = Rectangle(717,15);
+  rects_[31] = Rectangle(732,16);
+  rects_[32] = Rectangle(748,12);
+  rects_[33] = Rectangle(760,24);
+  rects_[34] = Rectangle(784,24);
+  rects_[35] = Rectangle(808,16);
+  rects_[36] = Rectangle(824,18);
+  rects_[37] = Rectangle(842,12);
+  rects_[38] = Rectangle(854,36);
+  rects_[39] = Rectangle(890,9);
+  rects_[40] = Rectangle(899,22);
+  rects_[41] = Rectangle(921,12);
+  rects_[42] = Rectangle(933,32);
+  rects_[43] = Rectangle(965,20);
+  rects_[44] = Rectangle(985,16);
+  rects_[45] = Rectangle(1001,22);
+  rects_[46] = Rectangle(1023,14);
+  rects_[47] = Rectangle(1037,16);
+  rects_[48] = Rectangle(1053,6);
+  // end of rectangles
+  
   int gen_off = 0;
   Serial.print("leds is at: ");
   Serial.println((int)leds_);
---
+++ booth_controller/rect_gen.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+
+
+if __name__ == "__main__":
+  # write to temp generate file
+  with open("temp", "w") as f:
+    filename = 'rects.csv'
+    data = pd.read_csv(filename)
+    # print(data)
+
+    start_ind = data.iloc[:49, 13].tolist()
+    rect_len = data.iloc[:49, 12].tolist()
+
+    temp_str = lambda ind, val1, val2 : f"rects_[{int(ind)}] = Rectangle({int(val1)},{int(val2)});\n"
+    template_str = "rects_[] = Rectangle(, )"
+    for i in range(len(rect_len)):
+        f.write(temp_str(i, start_ind[i], rect_len[i]))
+  
+
+  # find start and end positon
+  start_line = 0
+  end_line = 0 
+  i = 0
+  with open("lighting_array_controller.cpp", "r") as source_code:
+    lines = source_code.readlines()
+  with open("lighting_array_controller.cpp", "r") as source_code:
+    for line in lines:
+      # print(line)
+      if "// setup rectangles" in line:
+        start_line = i+1
+        print(f"start is: {i}")
+      elif "// end of rectangles" in line:
+        end_line =i-1
+        print(f"end is: {i}")
+      i +=1
+  # remove existing text in start and end position
+  with open("lighting_array_controller.cpp", "w") as source_code:
+    i = 0
+    for line in lines:
+      if (i < start_line or i > end_line):
+        source_code.write(line)
+      i += 1
+
+  # insert nice new lines
+  with open("lighting_array_controller.cpp", "w") as source_code:
+    with open("temp", "r") as temp:
+      i = 0
+      done = False
+      for line in lines:
+        
+        if (i > start_line - 1 and not done):
+          for new_line in temp.readlines():
+            source_code.write("  " + new_line)
+            i += 1
+            done = True
+          source_code.write("  // end of rectangles\n")
+        else:
+          source_code.write(line)
+        i += 1
+
+
