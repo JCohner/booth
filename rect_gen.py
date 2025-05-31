6f071aeb5553d931a47108030f4441d826904717
jambox <joshuacohen2019@u.northwestern.edu>
2025-05-30 22:33:56 -0700

    feat: organzied so we can have it do the h file too

--- rect_gen.py
+++ rect_gen.py
@@ -3,63 +3,55 @@ import sys
 import pandas as pd
 import numpy as np
 
-def populate_rect_section(fname_prefix: str, rect_start: int, rect_end: int):
-  fname = fname_prefix + "lighting_array_controller.cpp"
+def write_rect_values_to_temp(rect_start: int, rect_end: int):
+    with open("temp", "w") as f:
+        filename = 'rects.csv'
+        data = pd.read_csv(filename)
+        # print(data)
 
-  '''
-  open and write c code to temp file 
-  ''' 
-  with open("temp", "w") as f:
-    filename = 'rects.csv'
-    data = pd.read_csv(filename)
-    # print(data)
+        '''
+        Parse out rectangle length (in LEDs) and its assigned number
+        '''
+        rect_len = data.iloc[rect_start:rect_end, 12].tolist()
+        og_num = data.iloc[rect_start:rect_end, 0].tolist()
+        accumulator_ind = 0
 
-    '''
-    Parse out rectangle length (in LEDs) and its assigned number
-    '''
-    rect_len = data.iloc[rect_start:rect_end, 12].tolist()
-    og_num = data.iloc[rect_start:rect_end, 0].tolist()
-    accumulator_ind = 0
+        '''
+        string builder
+        '''
+        temp_str = lambda ind, val1, val2, og_num : \
+          f"rects_[{int(ind)}] = Rectangle({int(val1)},{int(val2)});  // rect #{og_num}\n"
 
-    '''
-    string builder
-    '''
-    temp_str = lambda ind, val1, val2, og_num : \
-      f"rects_[{int(ind)}] = Rectangle({int(val1)},{int(val2)});  // rect #{og_num}\n"
-    
-    for i in range(len(rect_len)):
-        f.write(temp_str(i, accumulator_ind, rect_len[i], og_num[i]))
-        accumulator_ind += rect_len[i]
-  
-  '''
-  open relevant controller cpp file and
-  * find start and end positon
-  * remove existing code
-  * populate with generated code
-  '''
-  start_line = 0
-  end_line = 0 
-  i = 0
-  with open(fname, "r") as source_code:
-    lines = source_code.readlines()
-  with open(fname, "r") as source_code:
-    for line in lines:
-      # print(line)
-      if "// setup rectangles" in line:
-        start_line = i+1
-        print(f"start is: {i}")
-      elif "// end of rectangles" in line:
-        end_line =i-1
-        print(f"end is: {i}")
-      i +=1
-  # remove existing text in start and end position
-  with open(fname, "w") as source_code:
+        for i in range(len(rect_len)):
+            f.write(temp_str(i, accumulator_ind, rect_len[i], og_num[i]))
+            accumulator_ind += rect_len[i]
+
+def delete_lines_between_flags(fname: str, start_flag: str, end_flag: str):
+    start_line = 0
+    end_line = 0 
     i = 0
-    for line in lines:
-      if (i < start_line or i > end_line):
-        source_code.write(line)
-      i += 1
+    with open(fname, "r") as source_code:
+        lines = source_code.readlines()
+    with open(fname, "r") as source_code:
+        for line in lines:
+            # print(line)
+            if start_flag in line:
+                start_line = i+1
+                print(f"start flag: {start_flag} is at line: {i}")
+            elif end_flag in line:
+                end_line =i-1
+                print(f"end flag: {end_flag} is at line: {i}")
+            i +=1
+    # remove existing text in start and end position
+    with open(fname, "w") as source_code:
+        i = 0
+        for line in lines:
+            if (i < start_line or i > end_line):
+                source_code.write(line)
+            i += 1
+    return start_line
 
+def write_temp_file_between_flags(fname:str, start_line: int, end_flag: str):
   # have to reopen the file because we modified the lines!
   with open(fname, "r") as source_code:
     lines = source_code.readlines()
@@ -73,16 +65,22 @@ def populate_rect_section(fname_prefix: str, rect_start: int, rect_end: int):
           for new_line in temp.readlines():
             source_code.write("  " + new_line)
             done = True
-          source_code.write("  // end of rectangles\n")
+          source_code.write(f"  // end of rectangles\n")
         else:
           source_code.write(line)
         i += 1
 
 
+def populate_rect_section(fname_prefix: str, rect_start: int, rect_end: int):
+    fname = fname_prefix + "lighting_array_controller.cpp"
+    write_rect_values_to_temp(rect_start, rect_end)
+    start_line = delete_lines_between_flags(fname, "  // setup rectangles", "  // end of rectangles")
+    write_temp_file_between_flags(fname, start_line, "  // end of rectangles")
+
+
+
 if __name__ == "__main__":
-    #print(f"{sys.argv[1]}, {int(sys.argv[2])}, {int(sys.argv[3])}")
     populate_rect_section(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-    # populate_rect_section("led_driver/", 29, 49)
     
 
 
